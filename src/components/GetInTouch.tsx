343903015f28ce416e7a03ac957cf54f0c5905ec
amos-babu <amos.babu@yahoo.com>
2025-07-19 09:21:50 +0300

    remove motion

--- src/components/GetInTouch.tsx
+++ src/components/GetInTouch.tsx
@@ -1,27 +1,11 @@
 import contacts from "../data/contacts.json";
-import { motion } from "framer-motion";
 
 export const GetInTouch = () => {
-  const iconVariants = (duration: number) => ({
-    initial: { y: -10 },
-    animate: {
-      y: [10, -10],
-      transition: {
-        duration: duration,
-        ease: "linear",
-        repeat: Infinity,
-        repeatType: "reverse" as "reverse",
-      },
-    },
-  });
 
   return (
     <div className="flex flex-wrap items-center justify-center gap-4 mt-4">
       {contacts.map((contact) => (
-        <motion.a
-          variants={iconVariants(contact.iconVariants)}
-          initial="initial"
-          animate="animate"
+        <a
           target="_blank"
           key={contact.id}
           href={contact.link}
@@ -32,7 +16,7 @@ export const GetInTouch = () => {
             alt="LinkedIn Logo"
             className="self-center w-5 h-5 justify-self-center"
           />
-        </motion.a>
+        </a>
       ))}
     </div>
   );
