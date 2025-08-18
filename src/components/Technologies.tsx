aea4e7117100d213caa7f9668d7bcdab84526cb6
amos-babu <amos.babu@yahoo.com>
2025-08-18 14:42:38 +0300

    animations

--- src/components/Technologies.tsx
+++ src/components/Technologies.tsx
@@ -17,40 +17,54 @@ export const Technologies = () => {
   });
 
   return (
-    <motion.div
-      className="flex flex-col mx-5"
-      initial={{ opacity: 0, y: 40 }}
-      whileInView={{ opacity: 1, y: 0 }}
-      transition={{ duration: 1.0 }}
-      viewport={{ once: true }}
-    >
-      <div>
+    <div className="flex flex-col mx-5">
+      <motion.div
+        initial={{ opacity: 0, y: 40 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        transition={{ duration: 1.0 }}
+        viewport={{ once: true }}
+      >
         <p className="mt-20 mb-5 font-mono text-xl text-center">
           <span className="text-2xl font-bold">Languages</span>
         </p>
-      </div>
+      </motion.div>
       <div className="flex flex-wrap justify-center gap-20">
         {languages.map((language) => (
-          <div className="max-w-3xl text-center" key={language.id}>
+          <motion.div
+            initial={{ opacity: 0, y: 40 }}
+            whileInView={{ opacity: 1, y: 0 }}
+            transition={{ duration: 1.0 }}
+            viewport={{ once: true }}
+            className="max-w-3xl text-center"
+            key={language.id}
+          >
             {" "}
             <img
               src={language.path}
               alt={language.name}
               className="w-20 h-20"
             />
-          </div>
+          </motion.div>
         ))}
       </div>
-      <div>
+      <motion.div
+        initial={{ opacity: 0, y: 40 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        transition={{ duration: 1.0 }}
+        viewport={{ once: true }}
+      >
         <p className="mt-20 mb-5 font-mono text-xl text-center">
           <span className="text-2xl font-bold">Frameworks and Tools</span>
         </p>
-      </div>
+      </motion.div>
       <div className="flex flex-wrap justify-center gap-20">
         {tools.map((tool) => (
           <motion.div
             variants={iconVariants(tool.iconVariants)}
-            initial="initial"
+            initial={{ opacity: 0, y: 40 }}
+            whileInView={{ opacity: 1, y: 0 }}
+            transition={{ duration: 1.0 }}
+            viewport={{ once: true }}
             animate="animate"
             className="max-w-2xl text-center"
             key={tool.id}
@@ -60,6 +74,6 @@ export const Technologies = () => {
           </motion.div>
         ))}
       </div>
-    </motion.div>
+    </div>
   );
 };
